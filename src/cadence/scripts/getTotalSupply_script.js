bac8c7243baa956fe34e9b6cad68480656ee9c34
Mohammed Habeb <mohammed0habeb@gmail.com>
2022-06-26 21:14:59 +0100

    first upload

--- src/cadence/scripts/getTotalSupply_script.js
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -1,11 +1,11 @@
 export const getTotalSupply =
-// REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-`
-import BottomShot from 0x7b6adb682517f137;
+  // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+  `
+import BottomShot from 0x3e5c4324fbfb06ac;
 
 pub fun main(): UInt64 {
 
     return BottomShot.totalSupply;
 
 }
-`
+`;
